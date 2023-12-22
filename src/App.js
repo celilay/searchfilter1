0e91904687a2a6cdf57509b19053e2152ce6b4e7
celilay <celilay34@gmail.com>
2023-12-22 22:41:53 +0300

    proje tamamlandı

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import { useState } from "react";
+import { Users } from "./users";
+import Table from "./Table";
 function App() {
-  return <>Merhaba</>;
+const [query,setQuery] = useState("");
+const keys = ["first_name","last_name","email"];
 
-  
+
+const search = (data) =>{
+  return data.filter((item) =>
+    keys.some((key) => item[key].toLowerCase().includes(query))
+  );
+  };
+
+  return (
+    <div className="app">
+      <input type="text" placeholder="Search..." className="search" onChange={e=> setQuery(e.target.value)}/>
+      <Table data={search(Users)}/>
+
+    </div>
+  );
 }
 
 export default App;
